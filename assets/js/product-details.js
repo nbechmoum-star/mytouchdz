75d5fda5335e9c9fda5e7d3bf0aa2104b1c184b3
bechmoumnouh-del <bechmoumnouh@gmail.com>
2026-08-10 20:58:57 +0100

    Update product-details.js

--- assets/js/product-details.js
+++ assets/js/product-details.js
@@ -68,8 +68,8 @@ const PRODUCT_DETAILS = {
     {
       id: 1,
       qty: 1,
-      price: 3500,
-      originalPrice: 4200,
+      price: 5"4000,
+      originalPrice: 5200
       freeDelivery: false,
       title: 'قطعة واحدة',
       active: true, // pre-selected on page load
@@ -77,16 +77,16 @@ const PRODUCT_DETAILS = {
     {
       id: 2,
       qty: 2,
-      price: 6900,
-      originalPrice: 8400,
+      price: 7900,
+      originalPrice: 104000,
       freeDelivery: false,
       title: 'قطعتان',
     },
     {
       id: 3,
       qty: 3,
-      price: 9900,
-      originalPrice: 12600,
+      price: 11500,
+      originalPrice: 15600,
       freeDelivery: false,
       title: 'ثلاث قطع',
     },
