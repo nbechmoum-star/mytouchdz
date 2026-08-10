f86229922eb7be483e2e57286981021290c258b6
bechmoumnouh-del <bechmoumnouh@gmail.com>
2026-08-10 22:06:14 +0100

    Add files via upload

--- assets/js/product-details.js
+++ assets/js/product-details.js
@@ -68,8 +68,8 @@ const PRODUCT_DETAILS = {
     {
       id: 1,
       qty: 1,
-      price: 3500,
-      originalPrice: 4200,
+      price: 4000,
+      originalPrice: 4800,
       freeDelivery: false,
       title: 'قطعة واحدة',
       active: true, // pre-selected on page load
@@ -77,16 +77,16 @@ const PRODUCT_DETAILS = {
     {
       id: 2,
       qty: 2,
-      price: 6900,
-      originalPrice: 8400,
+      price: 7500,
+      originalPrice: 9600,
       freeDelivery: false,
       title: 'قطعتان',
     },
     {
       id: 3,
       qty: 3,
-      price: 9900,
-      originalPrice: 12600,
+      price: 11000,
+      originalPrice: 14400,
       freeDelivery: false,
       title: 'ثلاث قطع',
     },
